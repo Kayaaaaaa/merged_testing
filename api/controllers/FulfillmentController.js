92255969d648849a9c59e3453b3f8333f8b6ff1e
Kenny Cheng <kenny@portable.red-server.net>
2019-01-30 13:09:24 +0800

    move fulfillment to custom response.

--- api/controllers/FulfillmentController.js
+++ api/controllers/FulfillmentController.js
@@ -4,31 +4,9 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
-
 module.exports = {
   index: async function (req, res) {
-    const agent = new WebhookClient({ req, res });
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-    function welcome (agent) {
-      agent.add(`Welcome to my agent!`);
-    }
-
-    function fallback (agent) {
-      agent.add(`I didn't understand`);
-      agent.add(`I'm sorry, can you try again?`);
-    }
-
-    let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-  // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-  // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
-    return agent.handleRequest(intentMap);
+    return res.fulfillment();
   }
 };
 
---
+++ api/responses/fulfillment.js
@@ -0,0 +1,29 @@
+
+module.exports = function() {
+
+  var req = this.req;
+  var res = this.res;
+
+  const {WebhookClient} = require('dialogflow-fulfillment');
+  const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+  const agent = new WebhookClient({ req, res });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome (agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback (agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+  // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+}
