d10b8450c8e246c1ea211a9c1225624d1827d305
e <18218067@life.hkbu.edu.hk>
2019-03-14 14:12:50 +0800

    connected Firebase

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -1,22 +1,50 @@
+import firebase from "firebase";
+var firebase = require("firebase");
+src="https://www.gstatic.com/firebasejs/5.8.6/firebase.js"
 
-module.exports = function() {
+var config = {
+  apiKey: "AIzaSyATvX8K5MUO6PNF_44rSPW2o9FGQSEJu1w",
+  authDomain: "wecarebill-92132.firebaseapp.com",
+  databaseURL: "https://wecarebill-92132.firebaseio.com",
+  projectId: "wecarebill-92132",
+  storageBucket: "wecarebill-92132.appspot.com",
+  messagingSenderId: "45479658617"
+};
+firebase.initializeApp(config);
+
+module.exports = function () {
 
   var req = this.req;
   var res = this.res;
 
-  const {WebhookClient} = require('dialogflow-fulfillment');
-  const {Card, Suggestion} = require('dialogflow-fulfillment');
+  const { WebhookClient } = require('dialogflow-fulfillment');
+  // const {Card, Suggestion} = require('dialogflow-fulfillment');
 
-  const agent = new WebhookClient({request: req, response: res});
+  const agent = new WebhookClient({ request: req, response: res });
 
-  function welcome (agent) {
+  function welcome(agent) {
     agent.add(`Welcome to my agent!`);
   }
 
-  function fallback (agent) {
+  function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
   }
+
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
 
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
@@ -24,4 +52,6 @@ module.exports = function() {
   // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
   // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
   agent.handleRequest(intentMap);
+
+  
 }
