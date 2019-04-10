8eb2ccede967f068f2168cd1d893cbb70cdf5e85
e <18218067@life.hkbu.edu.hk>
2019-04-10 22:29:19 +0800

    modified csv column

--- api/controllers/FulfillmentController.js
+++ api/controllers/FulfillmentController.js
@@ -20,19 +20,39 @@ module.exports = {
     // import * as admin from 'firebase-admin';
 
     var db = sails.firebaseAdmin.firestore();
-    var surgery = await db.collection('surgery').get()
+    // var surgery = await db.collection('surgery').get()
+    var user_record = await db.collection('user_record').get()
+    // var user_record_id = await db.collection('user_record').doc();
+    // console.log(user_record_id);
     //var optionA = await db.collection('surgery').doc('34').get()
     // console.log(surgery);
 
     var tempArray = [];
 
-    surgery.forEach(function (snapshot) {
+    // user_record_id.forEach(function (snapshot){
+    //   var data = snapshot.data()
+    //   tempArray.push({
+    //     id: data.id
+    //   })
+    // })
+    // .catch(err => {
+    //   console.log('Error getting documents', err);
+    // });
+
+    user_record.forEach(function (snapshot) {
       var data = snapshot.data()
       tempArray.push({
-        id: data.id,
-        title: data.title,
-        content: data.content,
-        specialist: data.specialist
+        doctorName: data.doctorName,
+        hospitalName: data.hospitalName,
+        otherHospitalName: data.otherHospitalName,
+        totalCost:  data.totalCost,
+        surgeryName: data.surgeryName,
+        surgeryNo: data.surgeryNo,
+        generalCode: data.generalCode,
+        specificCode: data.specificCode,
+        specialCaseComplex: data.specialCaseComplex,
+        detailOfSpecficC: data.detailOfSpecficC
+
       })
     });
 
